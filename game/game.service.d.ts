912bc79af95938cc974d3378b55dbb3e1b09470b
Cuong <loutrinh2312000@gmail.com>
2025-08-20 10:27:53 +0700

    Updates

--- game/game.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Model } from 'mongoose';
-import { Game, GameDocument } from './schemas/game.schema';
-export declare class GameService {
-    private gameModel;
-    constructor(gameModel: Model<GameDocument>);
-    create(winner: string, moves: number): Promise<Game>;
-}
